171dcdb2b321f5c1f6e470d96c1dd1367923ccd5
sdl-adhd-coder <96659361+sdl-adhd-coder@users.noreply.github.com>
2022-01-07 12:45:42 +0900

    Added -Scaffold --bgcolor --appbar --body ---image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I AM RICH'),
+          backgroundColor: Colors.blueGrey[800],
+        ),
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2021/04/dnce6n-e1618929428972.jpg'),
+          ),
+        ),
       ),
     ),
   );
